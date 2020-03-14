b6ecad99cd16037ce952b9b67d5ba4487f008a2f
Arpita-Mahapatra <62139226+Arpita-Mahapatra@users.noreply.github.com>
2020-03-14 09:12:34 +0530

    Add files via upload

---
+++ Area_task_14_03.py
@@ -0,0 +1,23 @@
+r=9.2
+h=3
+pi=(22/7)
+a=(1/3)*pi*(r**2)*h
+area=int(a)
+print("area of cone is", area)
+
+n=5
+p= 5*12
+r = 13/100
+npr= n*p*r
+print("the value of npr is", npr)
+
+r=3.2
+area=(22/7)*(r**2)
+print("Area of circle is", area)
+print(type(area))
+
+r=3.8
+circum=2*(22/7)*r
+print("Circumference of circle is", circum)
+print(type(circum))
+            
